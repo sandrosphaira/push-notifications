ca7f7b7d455393f4b4f3effa4f1305c2776e98ce
Sandro Augusto de Oliveira <sandro.oliveira@sphaira-i.com>
2023-02-16 01:44:03 -0300

    Discount notification

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
 self.addEventListener('push', a => {
   var jsonData = a.data.text()
   console.log(jsonData)
-  self.registration.showNotification('EUA!', {
+  self.registration.showNotification('Plague Inc: Envolved with 50% off', {
     title: 'Amazing!!',
-    body: 'This is the notification message',
+    body: 'Checkout this super discount!',
     vibrate: [200, 100, 200],
     sound: 'default',
-    icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz32ep-zpOj3w7jD3MmBJYEpdqzek8CJYbyg&usqp=CAU',
-    badge:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbWkEjBwGx_36HZqj1cwtI9ncdDtkDXH7QdQ&usqp=CAU',
+    image: 'https://content.planetplay.com/plague_logo.webp',
+    badge: 'https://content.planetplay.com/auth0-logo.webp',
     actions: [
       {
         action: 'reply',
