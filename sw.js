9a0e87d07113304504e40f8f4e83a9e734c3c8cc
Sandro Augusto de Oliveira <sandro.oliveira@sphaira-i.com>
2023-02-16 01:24:40 -0300

    Fix popup 20

--- sw.js
+++ sw.js
@@ -2,10 +2,11 @@ self.addEventListener('push', a => {
   var jsonData = a.data.text()
   console.log(jsonData)
   self.registration.showNotification('Joao!', {
-    title: 'Chique jovem',
+    title: 'Amazing!!',
     body: 'This is the notification message',
     vibrate: [200, 100, 200],
     sound: 'default',
+    icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz32ep-zpOj3w7jD3MmBJYEpdqzek8CJYbyg&usqp=CAU',
     actions: [
       {
         action: 'reply',
