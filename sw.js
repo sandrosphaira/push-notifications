3443ec0df57a865f891f1acda526fdc767ed22ec
Sandro Augusto de Oliveira <sandro.oliveira@sphaira-i.com>
2023-02-16 01:05:34 -0300

    Popup

--- sw.js
+++ sw.js
@@ -2,9 +2,17 @@ self.addEventListener('push', a => {
   var jsonData = a.data.text()
   console.log(jsonData)
   self.registration.showNotification('Jovem', {
-    title: 'My Notification Title',
+    title: 'Chique jovem',
     body: 'This is the notification message',
     vibrate: [200, 100, 200],
-    sound: 'default'
+    sound: 'default',
+    actions: [
+      {
+        action: 'reply',
+        type: 'text',
+        title: 'Reply',
+        placeholder: 'Type text here'
+      }
+    ]
   })
 })
