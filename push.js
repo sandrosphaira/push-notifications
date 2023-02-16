81f805fe48a60ddac9e356926d6290539209fd6a
Sandro Augusto de Oliveira <sandro.oliveira@sphaira-i.com>
2023-02-16 00:45:45 -0300

    Fix sound

--- push.js
+++ push.js
@@ -10,12 +10,12 @@ push.setVapidDetails('mailto:test@code.com', vapidKeys.publicKey, vapidKeys.priv
 
 let sub = {
   endpoint:
-    'https://fcm.googleapis.com/fcm/send/fm6slnWeRVQ:APA91bF_3r_NevvcVyKa6-0fyFXbSmDNoWsJSbrl_z32tdtE-zoFwGL_Y2cH71_JSO5AIyWkbf-I2qVrf4Gp19n2tXEXkVDOjh6TbfjAv6bpCjIXQyYug8-qJommCSPOcM1at7dPB7XC',
+    'https://fcm.googleapis.com/fcm/send/dN_9u10lID0:APA91bE7w0bcSVCYlrJN3rE_9DTo0caqO4D4aYalhd_03YckqlN_7jyL5wsktB_dVz2HCDoJOuglv-gtX3MAob4_flwdsRkP5sdnybwepp6p8nIy-LA3MdsItXkqVR50xjfVV_XAudYx',
   expirationTime: null,
   keys: {
     p256dh:
-      'BIR13Ly6CLms-fqu4uNmY_Ufiul8Y-f7jPvX8TdX0ezySJlHu9Qm3B4saiyuCHGP7ORbkNTHtS4nRykx7BOwEm8',
-    auth: 'UXsg80JFFAYAIxWRH9xa1g'
+      'BGf6Iq-F2SxiiZya1l8KP4Gk4m5cHxz9d8pETOEA6SgXovVzSfoFCfX5dqnzjo8CjewXymzDqsKMQkaTiS3ogSk',
+    auth: 'p03P4GriCsc6hgNpzaVC-w'
   }
 }
 
--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ self.addEventListener('push', a => {
   console.log(jsonData)
   self.registration.showNotification(jsonData, {
     body: 'Buzz! Buzz!',
+    sound: 'default',
     icon: 'https://play-lh.googleusercontent.com/yPtnkXQAn6yEahOurxuYZL576FDXWn3CqewVcEWJsXlega_nSiavBvmaXwfTGktGlQ',
     vibrate: [200, 100, 200, 100, 200, 100, 200],
     tag: 'vibration-sample'
