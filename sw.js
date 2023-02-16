ec5c9099ce609e7326e6952d29d20c7a8101d9d7
Sandro Augusto de Oliveira <sandro.oliveira@sphaira-i.com>
2023-02-16 00:25:25 -0300

    Fix 1004

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('push', a => {
-  var jsonData = JSON.parse(event.data.text())
+  var jsonData = JSON.parse(a.data.text())
   console.log(jsonData)
   self.registration.showNotification(a, {})
 })
