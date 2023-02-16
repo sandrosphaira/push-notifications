9dba460c69da68d5567ebe8d5fb4da1707569b62
Sandro Augusto de Oliveira <sandro.oliveira@sphaira-i.com>
2023-02-16 01:31:28 -0300

    Fix popup 220

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
 self.addEventListener('push', a => {
   var jsonData = a.data.text()
   console.log(jsonData)
-  self.registration.showNotification('Belezura pura!', {
+  self.registration.showNotification('EUA!', {
     title: 'Amazing!!',
     body: 'This is the notification message',
     vibrate: [200, 100, 200],
     sound: 'default',
     icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz32ep-zpOj3w7jD3MmBJYEpdqzek8CJYbyg&usqp=CAU',
+    badge:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbWkEjBwGx_36HZqj1cwtI9ncdDtkDXH7QdQ&usqp=CAU',
     actions: [
       {
         action: 'reply',
